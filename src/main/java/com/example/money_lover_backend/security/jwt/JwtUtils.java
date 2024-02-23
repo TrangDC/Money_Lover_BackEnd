b50180cd6ce19b819c282c0593032a18fae0bc2b
TrangDC <dinhcongtrang99@gmail.com>
2024-02-23 23:43:27 +0700

    Update JwtUtils class. Create TokenRefreshRequest and TokenRefreshResponse

--- src/main/java/com/example/money_lover_backend/security/jwt/JwtUtils.java
+++ src/main/java/com/example/money_lover_backend/security/jwt/JwtUtils.java
@@ -61,4 +61,12 @@ public class JwtUtils {
 
         return false;
     }
+
+    public String generateTokenFromUsername(String username) {
+        return Jwts.builder().setSubject(username).setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs)).signWith(SignatureAlgorithm.HS512, jwtSecret)
+                .compact();
+    }
+
+
 }
